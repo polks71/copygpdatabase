b57dd99695841cd7e194cdcc77aeea62655d4d0b
Ross Boe <Ross.Boe@trunorthdynamics.com>
2025-10-29 09:05:06 -0400

    latest

---
+++ src/CloneDatabase/CloneGPDatabase.Legacy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneGPDatabase.Legacy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceServer = ConfigurationManager.AppSettings["SourceServer"];
+        }
+    }
+}
---
+++ src/CloneDatabase/CloneGPDatabase.Legacy/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CloneGPDatabase.Legacy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CloneGPDatabase.Legacy")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c9bc3c7-ba51-4d5d-b28d-40069f5e62b0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
